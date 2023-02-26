83db5b034c1e6ee6fe160ab51b8ef4d128aba551
PaulinaCarcamo <108833481+PaulinaCarcamo@users.noreply.github.com>
2023-02-26 17:00:02 -0300

    Updating images source

--- src/data/aboutCards.js
+++ src/data/aboutCards.js
@@ -1,7 +1,7 @@
 export const aboutCards = [
     {
         title: 'Atmosphere',
-        img: 'https://uc37784239362f2500360a371c53.previews.dropboxusercontent.com/p/thumb/AB3IeTBN53Rb6VCxuU_Vy0e_aEQzbB7LmcS6tMopMCEMcMO2f3rF-So-gfxyKwbGhV3I3I1PsQeO-SyPkLg9VttGIlNy6zCJG-SL4FkH0yrYEpo_EPrD1lAC9ci2uzc19ZNKoymVvL6Nr2ZfKg1W-JL40oOJ2_SzoEtHBSBdvUQcVjRzuqHbht86tRIotfwOG8ZCXTHTc8bTTdN1-5xm-2jew6Y5HJxWV2txdu5D-e_xKweKBLq1s9d8bKtRsqph4S3Ao5Tb1kCgkEYMb7ijXJxJ35_EjlQQFZSPIZDlWu2KPKQUdI74XNCZupWftZIpaCLEb2-HQUVS25r7MznmwBDWFNeJkXIFih-_pRwWaRckLD8PThSMB0XQj8jqpQ3CLL444q5m_soJqRV5qS4tmaVv4yJTfKVcEcnPRk7bL7jqQw/p.png',
+        img: 'https://firebasestorage.googleapis.com/v0/b/testing-photo-upload.appspot.com/o/restaurant-images-upload%2Foldmusicpng.png?alt=media&token=a6d91277-670d-4b0b-9b26-9a6c71a8a148',
         description: [
             'Iconic Googie architecture that captivates the hearts of classic diner lovers.',
             'Characteristic late-50s soft light glowing style.'
@@ -11,7 +11,7 @@ export const aboutCards = [
     },
     {
         title: 'Classic',
-        img: 'https://uc8a5334309049e13cbb68a2e355.previews.dropboxusercontent.com/p/thumb/AB2s3mi2oG5ZuBPPnkdRLyOBchySE_Cpp6OIL4qMe8uP9cM0YFT9-Sud2cZR7-QK8d4mDQmwCghrhnFKOyLOrRi34snLR9HPr4Tcq361owADOuIzQENP3e2vy17ni9gh_DSczpcNMa0npAyfyDNZnKdzTXuvz4WnmicFhuCWQMJDSFgLaagxwsjUNK94L7P3ZmULeuA2MJIOmAMF96FkJxYc7p6euqZXSQ4NRIx5-EdG8isD2AsI4ZhkYKtGVApw0uoZIRLScm5W4UxLCyTH5f5F1YO1hOizFLn-akPyoaUmHXi0PKgsFqWMBbtJhlvyY5QHh13mSpIJi3X6nuV1yx1NctMc0P7JtQPoEK06GoCO7zH_FsLBMsbZCM9zE6ez_OsrCpoDGFYFSK3BpQ_6TPuLtfjg1xofugxvxjoZIlihmg/p.jpeg',
+        img: 'https://firebasestorage.googleapis.com/v0/b/testing-photo-upload.appspot.com/o/restaurant-images-upload%2Foldburgersign.jpg?alt=media&token=08ae698b-6f54-4f27-bead-8990402a7386',
         // subheader: 'Most popular',
         description: [
             'Transport back to a time when things were simpler.',
@@ -22,7 +22,7 @@ export const aboutCards = [
     },
     {
         title: 'Experience',
-        img: 'https://ucaf088fec5a256c53800132a42d.previews.dropboxusercontent.com/p/thumb/AB2yQqkPHW_2DzPDGJjshXDpUzEV7aaeaAnM_D0D2j0KbOere8glls3tqits1tSsqf0pMuupZU64TCbwgToyv-JEkGj2-L6OVHoSBb06IGaUDWtnG1aTXLjFWMjhbU1aQelQOqkcnCRDexThYBAJUNs_QAB7PJtURC_XrKY2KjvZ_X-KSFZQUqjAH1RKOcIDdgKd6TcTHoTcoehM5HWYACoaQhAdGnsNjiaJr0CUFIszpbM9tzmqcbU4RU8D6xYtqZwYNkx9HYtnZ7akIVPOSh0UC-zczC0J7MbcqWLEWnlTHMllyHMfgisb0kZzOVy-Slt9reIStpIi2uJVKFHTNmTLgASy0FeXFjpug72x05Uy_2CTThRB6Q2r4086np57mWbeiJ8vF8gfPKETsK5ZPaWujhIGqqyJADWZtSrd05-jKQ/p.jpeg',
+        img: 'https://firebasestorage.googleapis.com/v0/b/testing-photo-upload.appspot.com/o/restaurant-images-upload%2Foldcoffeesignretro.jpg?alt=media&token=0d5b52e0-657d-4117-8490-7f9623af346d',
         description: [
             'There s something nostalgic about a dining establishment that has been around when the golden era of diners was truly at its peak.',
         ],
--- src/data/mainMeals.js
+++ src/data/mainMeals.js
@@ -3,7 +3,7 @@ export const mainMeals =
     title: 'Diner Classic',
     description:
         "Handcrafted patties cooked to medium, with choice of fries, tater tots, a cup of soup, coleslaw or potato salad. Substitute a house or caesar salad, onion rings or a cup of chili.",
-    image: 'https://ucc2ac35b4d1fbe84731c4296b98.previews.dropboxusercontent.com/p/thumb/AB07f1kZYxiDxvZnEzDaX1jzEkFwhwSPbSUG4AQDm2kVmk65Ca7TM1zHVDjeYpAqhznvBEAm5h-PysLKP2lSIaXhyL7SIWcX-CX5oyam-VQjXvmONlvrvVh-7l2FXDionBHH2NPsY8OTuurj9v7S2E6JQop2KrdiNd8mgwlytMGCbSgwL1X9lghD5SzK15chok6km_sxB9l3NOiL_dSSydUGRLu0QyLjLsoEtYK_v7ne8VRlTf6uUGeAsg_KRIoKrvchXCbgErWt1zyOuObEYSoRmZJppR4O-FUBrpFUZlL5hvDIGgmu_tUjt_TBET3IhTSP_QTVyM3RzmeZXQ90Sbsyqgq-RdH5FFBgbeY3NpvFVk33Y-zJ1ax0YIjUnLTkw7Djo_7Acov7ka7v-0LD3V5Medd2ymwySp8N9hrZ6P464Q/p.jpeg',
+    image: 'https://firebasestorage.googleapis.com/v0/b/testing-photo-upload.appspot.com/o/restaurant-images-upload%2Fmiamidinermilkshake.jpg?alt=media&token=bbcc4e2a-5d36-4704-82c3-14787f74d40e',
     imageText: ' ',
     // linkText: 'Ask for Delivery',
 };
